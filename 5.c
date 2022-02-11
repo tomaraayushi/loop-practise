c18fc820b44eda24e116bed89c9573f1e0c6de99
Aayushi Tomar <aayushitomar1998@gmail.com>
2022-02-11 11:04:17 +0000

    Commit 5.c

---
+++ 5.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n = get_int("N: ");
+    int m = get_int("M: ");
+    for( int i = n; i<= m; i++)
+    {
+        if( i%2 != 0)
+        {
+            printf("%i\n", i);
+        }
+    }
+}
